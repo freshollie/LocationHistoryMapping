06035190a1e105654d5cc9d46c4db964105d6e7b
Oliver Bell <freshollie@gmail.com>
2016-12-06 20:46:51 +0000

    Changed to heatmap

--- Mapper.py
+++ Mapper.py
@@ -1,12 +1,10 @@
-import gmaps
-gmaps.configure('AIzaSyDkKaasomnWFDxMdRHM5QAxRbxzZIkIBaw')
-
+import heatmap
 locationList = []
 
 for line in open('locationList.txt').readlines():
     stringCoord = line.strip().split(',')
     locationList.append([float(stringCoord[0]), float(stringCoord[1])])
 
-m = gmaps.Map()
-layer = gmaps.Heatmap(data=locationList)
-m.add_layer(layer)
+h = heatmap.Heatmap()
+img = h.heatmap()
+img.save('test.png')
