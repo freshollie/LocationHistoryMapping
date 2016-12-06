167dcfe3f122b7e04f9eae8a3fcc29c602954817
Oliver Bell <freshollie@gmail.com>
2016-12-06 22:55:22 +0000

    Changed to google maps plotting using gmplot module to make an html from data

--- Mapper.py
+++ Mapper.py
@@ -1,10 +1,32 @@
-import heatmap
-locationList = []
+import gmplot
+from haversine import haversine
 
-for line in open('locationList.txt').readlines():
-    stringCoord = line.strip().split(',')
-    locationList.append([float(stringCoord[0]), float(stringCoord[1])])
 
-h = heatmap.Heatmap()
-img = h.heatmap()
-img.save('test.png')
+def getCoordsInRadius(centreLat, centreLong, radius = 10, every = 1): # km radius
+
+    x = []
+    y = []
+    i = 0
+    for line in open('locationList.txt').readlines():
+        stringCoord = line.strip().split(',')
+        lat = float(stringCoord[0])
+        long = float(stringCoord[1])
+
+        if i % every == 0 and haversine((centreLat, centreLong),
+                                        (lat, long)
+                                        ) <= radius:
+            x.append(lat)
+            y.append(long)
+        i += 1
+
+    return x, y
+
+
+lat, long = gmplot.GoogleMapPlotter.geocode("Crowthorne")
+gmap = gmplot.GoogleMapPlotter(lat, long, zoom = 10)
+
+
+xs, ys = getCoordsInRadius(lat, long, every = 20)
+gmap.scatter(xs, ys, marker=False)
+
+gmap.draw("mymap.html")
